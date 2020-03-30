32636db58ad531804f4528b5776e6b378110652e
Adrian Kuper <adrian.kuper@hotmail.de>
2020-03-30 15:01:41 +0200

    Scaffold all identity views

---
+++ TurnipExchange/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TurnipExchange.Data;
+
+[assembly: HostingStartup(typeof(TurnipExchange.Areas.Identity.IdentityHostingStartup))]
+namespace TurnipExchange.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
